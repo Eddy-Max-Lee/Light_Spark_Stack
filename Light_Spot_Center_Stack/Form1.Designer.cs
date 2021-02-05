e390c1f534bf0ee89733dbbc517ca9905bf79d35
Eddy Max, Lee <bysky5.44.32.164@gmail.com>
2021-02-05 18:34:39 +0800

    First

---
+++ Light_Spot_Center_Stack/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Light_Spot_Center_Stack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbx_ = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btn_stack = new System.Windows.Forms.Button();
+            this.cb_list = new System.Windows.Forms.ComboBox();
+            this.btn_load = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbx_
+            // 
+            this.pbx_.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pbx_.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbx_.Location = new System.Drawing.Point(417, 3);
+            this.pbx_.Name = "pbx_";
+            this.pbx_.Size = new System.Drawing.Size(615, 535);
+            this.pbx_.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx_.TabIndex = 0;
+            this.pbx_.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel1.Controls.Add(this.pbx_, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1035, 677);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.btn_stack, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.cb_list, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.btn_load, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 9;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11013F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11012F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11012F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11012F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11013F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11235F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11235F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11235F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11235F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(408, 535);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // btn_stack
+            // 
+            this.btn_stack.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_stack.Location = new System.Drawing.Point(3, 180);
+            this.btn_stack.Name = "btn_stack";
+            this.btn_stack.Size = new System.Drawing.Size(402, 53);
+            this.btn_stack.TabIndex = 3;
+            this.btn_stack.Text = "疊圖";
+            this.btn_stack.UseVisualStyleBackColor = true;
+            this.btn_stack.Click += new System.EventHandler(this.btn_stack_Click);
+            // 
+            // cb_list
+            // 
+            this.cb_list.FormattingEnabled = true;
+            this.cb_list.Location = new System.Drawing.Point(3, 121);
+            this.cb_list.Name = "cb_list";
+            this.cb_list.Size = new System.Drawing.Size(402, 23);
+            this.cb_list.TabIndex = 1;
+            // 
+            // btn_load
+            // 
+            this.btn_load.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_load.Location = new System.Drawing.Point(3, 62);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(402, 53);
+            this.btn_load.TabIndex = 0;
+            this.btn_load.Text = "載入資料夾";
+            this.btn_load.UseVisualStyleBackColor = true;
+            this.btn_load.Click += new System.EventHandler(this.btn_load_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(402, 25);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "請輸入目標資料夾";
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1035, 677);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Light Spot Center Stack";
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbx_;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button btn_load;
+        private System.Windows.Forms.ComboBox cb_list;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_stack;
+    }
+}
+
---
+++ Light_Spot_Center_Stack/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Light_Spot_Center_Stack
+{
+    public partial class Form1 : Form
+    {
+         private static Dictionary<string, Bitmap> DICT_BMP= new Dictionary<string, Bitmap>();
+
+        public Form1()
+        {
+            InitializeComponent();
+           // textBox1.KeyDown += btn_stack_Click;
+        }
+        #region Tool
+        //====Tool Start========================
+        private Bitmap StackBitmap(Dictionary<string, Bitmap> dict) {
+
+            var first = dict.First();
+          //  Bitmap stacked =  first.Value;
+            Bitmap stacked = new Bitmap(first.Value.Width, first.Value.Height);
+
+           double[,] R_stacking = new double[first.Value.Width, first.Value.Height];
+            double[,] G_stacking = new double[first.Value.Width, first.Value.Height];
+            double[,] B_stacking = new double[first.Value.Width, first.Value.Height];
+
+
+
+
+            //1. 相加
+            foreach (KeyValuePair<string, Bitmap> pair in dict) {
+                Bitmap ori_bmp = new Bitmap(pair.Value.Width, pair.Value.Height);
+             
+                ori_bmp = pair.Value;
+
+
+
+                for (int Pixel_x = 0; Pixel_x < ori_bmp.Width; Pixel_x++)//一直是0->input_image.Width
+                {
+                    for (int Pixel_y = 0; Pixel_y < ori_bmp.Height; Pixel_y++)//初始值為now_y掃到now_y+20
+                    {
+                        Color color_new =ori_bmp.GetPixel(Pixel_x, Pixel_y);
+                        int Red = color_new.R;
+                        int Green = color_new.G;
+                        int Blue = color_new.B;
+
+                        //          R_stacking[Pixel_x, Pixel_y] = R_stacking[Pixel_x, Pixel_y] + Red>0?Red:0;
+                    
+                            R_stacking[Pixel_x, Pixel_y] = R_stacking[Pixel_x, Pixel_y] + (Red > 10 ? Red : 0);
+                            G_stacking[Pixel_x, Pixel_y] = G_stacking[Pixel_x, Pixel_y] + (Green > 10 ? Green : 0);
+                            B_stacking[Pixel_x, Pixel_y] = B_stacking[Pixel_x, Pixel_y] + (Blue > 10 ? Blue : 0);
+
+                   
+                    }
+                }
+            }
+            //2. 畫圓心
+            foreach (KeyValuePair<string, Bitmap> pair in dict)
+            {
+                Bitmap ori_bmp = new Bitmap(pair.Value.Width, pair.Value.Height);
+                string ori_name = pair.Key;
+                ori_bmp = pair.Value;
+
+                char sp0 = '(';
+                char sp2 = ')';
+                char sp3 = '_';
+
+                string center_string = ori_name.Split(sp0)[1].Split(sp2)[0];//()誇號中間的串
+                int centerX = Convert.ToInt32(center_string.Split(sp3)[0]);
+                int centerY = Convert.ToInt32(center_string.Split(sp3)[1]);
+                int thick = 5;
+               
+
+                for (int Pixel_x = 0; Pixel_x < ori_bmp.Width; Pixel_x++)
+                {
+                    for (int Pixel_y = 0; Pixel_y < ori_bmp.Height; Pixel_y++)
+                    {
+                        if (Math.Abs(Pixel_x - centerX)< thick && Math.Abs(Pixel_y - centerY) < thick)
+                        {                  
+                            R_stacking[Pixel_x, Pixel_y] = 255;
+                            G_stacking[Pixel_x, Pixel_y] = 0;
+                            B_stacking[Pixel_x, Pixel_y] = 0;
+                        } 
+                    }
+                }
+            }
+
+            //3. 將疊圖的值存入stack
+            for (int Pixel_x = 0; Pixel_x < first.Value.Width; Pixel_x++)//一直是0->input_image.Width
+            {
+                for (int Pixel_y = 0; Pixel_y < first.Value.Height; Pixel_y++)//初始值為now_y掃到now_y+20
+                {
+                    int Red = Convert.ToInt32(R_stacking[Pixel_x, Pixel_y] <=255? R_stacking[Pixel_x, Pixel_y ]:255 );
+                    int Green = Convert.ToInt32(G_stacking[Pixel_x, Pixel_y] <= 255 ? G_stacking[Pixel_x, Pixel_y] : 255); ;
+                    int Blue = Convert.ToInt32(B_stacking[Pixel_x, Pixel_y] <= 255 ? B_stacking[Pixel_x, Pixel_y] : 255);
+
+                    Color color_stack = Color.FromArgb(255,Red, Green, Blue);
+                    stacked.SetPixel(Pixel_x, Pixel_y, color_stack);
+                }
+            }
+
+            return stacked;
+        }
+
+
+
+        //===Tool End=======================
+        #endregion
+        private void btn_load_Click(object sender, EventArgs e)
+        {
+
+            // open file dialog   
+            FolderBrowserDialog open = new FolderBrowserDialog();
+            // image filters  
+           // open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                DirectoryInfo dir0 = new DirectoryInfo(open.SelectedPath);
+
+                FileInfo[] files = dir0.GetFiles();
+                StringBuilder sb = new StringBuilder();
+                textBox1.Text=open.SelectedPath;
+
+                foreach (FileInfo file in files)
+                {
+                //    sb.Append("<img src='' data-original='_img/bookImg/" + bookNo + "/" + file.Name + "' style='width:100%;'  />");
+                    if(   file.Name.Contains("_stamped.bmp"))  cb_list.Items.Add(file.Name);
+                }
+             //   bookImg.InnerHtml = sb.ToString();
+
+
+
+        //        char sp1 = '\\';
+           //     char sp2 = '.';
+              //  string name_buff = open.FileName.Split(sp1)[open.FileName.Split(sp1).Length - 1];
+               // tb_lens.Text = name_buff.Split(sp2)[0];
+
+
+            }
+        }
+
+        private void textBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Enter)
+            {
+
+                DirectoryInfo dir0 = new DirectoryInfo(textBox1.Text);
+
+                FileInfo[] files = dir0.GetFiles();
+                StringBuilder sb = new StringBuilder();
+                foreach (FileInfo file in files)
+                {
+                  //  if (file.Name.Contains("stamped.bmp"))  
+                        cb_list.Items.Add(file.Name);
+                }
+               // textBox1.KeyDown += btn_stack_Click;
+            }
+        }
+
+
+       
+        private void btn_stack_Click(object sender, EventArgs e)
+        {
+            DICT_BMP.Clear();
+            for (int i = 0; i < cb_list.Items.Count; i++) {
+                //  string path ="@"+ textBox1.Text + cb_list.Items[i].ToString());
+                string path =  textBox1.Text+"\\"+ cb_list.Items[i].ToString();
+                 Bitmap bmp = new Bitmap(path);
+                
+                DICT_BMP.Add(cb_list.Items[i].ToString(), bmp);
+                
+               // pbx_.Image = bmp;
+                //Thread.Sleep(500);
+            }
+
+            pbx_.Image = StackBitmap(DICT_BMP);
+        }
+    }
+}
